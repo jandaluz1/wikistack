66d0f7c0ba6fec81d6e86f819ba2e0adc9fbad3d
Joe Andaluz <jandaluz1@gmail.com>
2018-06-15 15:39:39 -0400

    working on POST wiki

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const addPage = require('../views/addPage');
+const { Page } = require('../models');
 
 router.get('/', (req, res) => {
-  res.send(`<h1>Wiki/</h1>`);
+  res.redirect('..');
 });
 router.get('/add', (req, res) => {
-  res.send('<h1>Wiki/add</h1>');
+  res.send(addPage());
 });
-router.post('/', (req, res) => {
-  res.send(req.body);
+router.post('/', async (req, res, next) => {
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  try {
+    await page.save();
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
